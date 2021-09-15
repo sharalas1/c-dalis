2b5c18ce9db9d2f5011cd81feb92ef6c579023db
shalaras1 <sarunasvildziunas@gmail.com>
2021-09-14 19:29:29 -0700

    nd

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var name = Console.ReadLine(); var yra dalykas kur nustato pats ar string ar int ir pns
+            Console.WriteLine("koks tavo vardas?");
+            string name = Console.ReadLine();
+            Console.WriteLine("kokia tavo pavarde?");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("koks tavo amzius?");
+            string age = Console.ReadLine();
+            Console.WriteLine(name +" " + lastname +" " + age);
+            //Console.WriteLine(string.Concat(name, " ", lastname " ", age)); tas pats eilutei rasyti
+            //Console.WriteLine(string.Format("{0} {1} {2}", name, lastname, age)); irgi eilutei rasyti budas
+            //Console.WriteLine($"Jus suvedete: {name} {lastname} {age}"); eilutei rasyti budas trumpiausias
+
+
+        }
+    }
+}
---
+++ uzduotis1.2/uzduotis1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uzduotis1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite atstuma metrais");
+            var m = Int32.Parse(m), Console.ReadLine();
+            Console.WriteLine("Iveskite laika sekundemis");
+            var s = Int32.Parse(s, Console.ReadLine();
+            Console.WriteLine = ("greitis:" + m / s);
+
+        }
+    }
+}
