224f6187ec8f5376f89136474cf015374a54ee4a
shalaras1 <sarunasvildziunas@gmail.com>
2021-09-15 22:00:43 -0700

    sviesaforas

---
+++ uzduotis2.3/uzduotis2.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace sviesaforas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trafficLight = Console.ReadLine();
+            var option = "";
+            switch (trafficLight)
+            {
+                case "#ff0a0a":
+                    option = "vaziuoti draudziama";
+                    break;
+                case "#ffe20a":
+                    option = "pasiruoskite";
+                    break;
+                case "#008509":
+                    option = "vaziuoti galima";
+                    break;
+                default:
+                    Console.WriteLine("Neatpazinta spalva");
+                    break;
+            }
+            if (option != "")
+            {
+                Console.WriteLine(option);
+            }
+        }
+    }
+}
